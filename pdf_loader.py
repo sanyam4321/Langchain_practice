0d2811c72185f9b30f39b75b60c6704fa4499765
sanyam <sanyam9922@gmail.com>
2025-07-15 16:46:32 +0530

    pdf loader

---
+++ pdf_loader.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv() 
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+loader = PyPDFLoader('git-cheat-sheet-education.pdf')
+docs = loader.load()
+print(len(docs))
