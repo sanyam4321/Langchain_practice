4d9bea1af56be85af0c836c1b4172c74b8482905
sanyam <sanyam9922@gmail.com>
2025-07-17 21:50:58 +0530

    webloader example

--- web_loader.py
+++ web_loader.py
@@ -1,9 +1,27 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv()
 
-url = 'https://www.amazon.com/'
-loader = WebBaseLoader(url)
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+prompt = PromptTemplate(
+    template='Answer the following question: {question}, based on the following text: {text}',
+    input_variables=['question', 'text']
+)
+parser = StrOutputParser()
+
+url = 'https://www.amazon.in/Daikin-Fixed-Copper-Filter-FTL28U/dp/B09R4SF5SP/ref=sr_1_1?crid=3OKX9EYC9ELDB&dib=eyJ2IjoiMSJ9.ZeLsLNuo30ALNIqMduy5lsGOLtFRZj3crFrYga3_VjaS7fHF39KCLyJX2utp3b54wv9yJgND7u6S85Wy4uVUcsAd-YeswoCMxRWY3tRdW_4.-NDhdwIXuh1MMWrOxGdyY8mhuAAAvK3HH1xxxhQhXzc&dib_tag=se&keywords=air+conditioner+1.5+ton&nav_sdd=aps&qid=1752769090&refinements=p_n_feature_eleven_browse-bin%3A2753096031&rnid=2753095031&s=kitchen&sprefix=air+con&sr=1-1'
+loader = WebBaseLoader(url) # it can even get multiple webpages and we can pass list of urls
+
+chain = prompt | model | parser
 
 docs = loader.load()
-print("length", len(docs))
 
-print(docs)
+output = chain.invoke({
+    'question': 'what is the specification of the product',
+    'text': docs[0].page_content
+})
+
+print(output)
