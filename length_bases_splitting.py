60aa1bb0d09f1137fd3d32fe035104aa6868047b
sanyam <sanyam9922@gmail.com>
2025-07-18 20:22:44 +0530

    length-splitting

--- length_bases_splitting.py
+++ length_bases_splitting.py
@@ -2,13 +2,14 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 loader = PyPDFLoader('books\Mindset-The-New-Psychology-of-Success-Dweck.pdf') 
-docs = loader.load()
+docs = loader.lazy_load()
 
 splitter = CharacterTextSplitter(
-    chunk_size=50,
-    chunk_overlap=0,
+    chunk_size=200,
+    chunk_overlap=30,
     separator=''
 )
 
-output = splitter.split_text(text)
-print(output)
+output = splitter.split_documents(docs)
+print(output[0].page_content)
+print(len(output))
