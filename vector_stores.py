f79c9b3e214c03d3ff04797b87a65ec61ebbc2db
sanyam <sanyam9922@gmail.com>
2025-07-19 22:05:32 +0530

    add documents in vector db

---
+++ vector_stores.py
@@ -0,0 +1,40 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+
+from langchain.schema import Document
+
+
+doc1 = Document(
+    page_content="Virat Kohli is one of the most successful and consistent batsmen in IPL history. Known for his aggressive batting style and leadership.",
+    metadata={"team": "Royal Challengers Bangalore"}
+)
+
+doc2 = Document(
+    page_content="Rohit Sharma is the most successful captain in IPL history, leading Mumbai Indians to five titles. He's known for his calm demeanor and powerful hitting.",
+    metadata={"team": "Mumbai Indians"}
+)
+
+doc3 = Document(
+    page_content="MS Dhoni, famously known as Captain Cool, has led Chennai Super Kings to multiple IPL titles. His finishing skills, wicketkeeping, and leadership are legendary.",
+    metadata={"team": "Chennai Super Kings"}
+)
+
+doc4 = Document(
+    page_content="Jasprit Bumrah is considered one of the best fast bowlers in T20 cricket. Playing for Mumbai Indians, he is known for his deadly yorkers and accuracy.",
+    metadata={"team": "Mumbai Indians"}
+)
+
+doc5 = Document(
+    page_content="Ravindra Jadeja is a dynamic all-rounder who contributes with both bat and ball. Representing Chennai Super Kings, his fielding is top-notch.",
+    metadata={"team": "Chennai Super Kings"}
+)
+
+docs = [doc1, doc2, doc3, doc4, doc5]
+
+vector_store = Chroma(
+    embedding_function=OllamaEmbeddings(model='nomic-embed-text:latest'),
+    persist_directory='chroma_db_vectors',
+    collection_name='players'
+)
+
+print(vector_store.add_documents(docs))
