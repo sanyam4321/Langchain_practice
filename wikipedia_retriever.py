0dd7833230ebe0ece766facf1846494f911ffc4f
sanyam <sanyam9922@gmail.com>
2025-07-20 20:20:37 +0530

    wikipedia retriever

---
+++ wikipedia_retriever.py
@@ -0,0 +1,11 @@
+from langchain_community.retrievers import WikipediaRetriever
+
+# retriever is a runnable
+retriever = WikipediaRetriever(top_k_results='2', lang='en')
+
+query = 'how black holes were discovered'
+docs = retriever.invoke(query)
+
+for i, doc in enumerate(docs):
+    print(f"---Result {i+1}---")
+    print(doc.page_content)
